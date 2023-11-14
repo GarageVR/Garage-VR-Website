9689c1da001be0a55ca7427c5912053febaac573
Fuat Can Akgün <fuatcanakgun@gmail.com>
2023-11-15 02:04:56 +0300

    Add mansion mystery page

--- src/routes/mansionMystery/+page.svelte
+++ src/routes/mansionMystery/+page.svelte
@@ -1,3 +1,137 @@
 <title>GarageVR: Mansion Mystery</title>
 
-<h1>Mansion Mystery</h1>
+<img src="mansionMysteryHeader.png" alt="Grill on Wheels logo" />
+<div class="contact-links">
+    <a href="/mm_oculus" target="_blank">
+        <img src="meta.png" alt="Meta icon" />
+    </a>
+    <a href="/mm_steam" target="_blank">
+        <img src="steam.png" alt="Discord icon" />
+    </a>
+    <a href="/mm_sidequest" target="_blank">
+        <img src="sidequest.png" alt="SideQuest icon" />
+    </a>
+    <a href="/mm_rift" target="_blank">
+        <img src="rift.png" alt="Oculus icon" />
+    </a>
+</div>
+
+<div>
+    <p>
+        <strong>An Unforgettable VR Escape Room Experience Awaits!</strong>
+        <br /><br />Embark on an enigmatic journey with "<em>Mansion Mystery</em
+        >". Navigate through the shadowy corridors and immerse yourself in a
+        puzzle-solving adventure where every decision is a key to unlock the
+        mystery. Dive deep into this VR escape and unravel the secrets of the
+        mansion.
+    </p>
+
+    <h2>Features:</h2>
+    <ul>
+        <li>
+            <strong>Immersive VR Gameplay:</strong> Experience every twist, turn,
+            and puzzle challenge in vivid VR.
+        </li>
+        <li>
+            <strong>Intricate Puzzles:</strong> Test your puzzle-solving skills with
+            carefully crafted challenges that keep you on the edge.
+        </li>
+        <li>
+            <strong>Interactive Environment:</strong> Collect items and interact
+            with the surroundings to unlock mysteries and escape the mansion.
+        </li>
+        <li>
+            <strong>Captivating Game Mechanics:</strong> Every clue matters, and
+            every second counts as you race against time to break free.
+        </li>
+    </ul>
+
+    <p>
+        Use your observational skills and creativity to navigate through the
+        mansion's puzzles. Each challenge is a piece of the larger mystery, and
+        every decision brings you closer to freedom. As you collect items and
+        interact with the environment, the mansion reveals its secrets. "<em
+            >Mansion Mystery</em
+        >" isn't just an escape room game; it's an immersive tale of suspense,
+        ingenuity, and the thrill of solving intricate puzzles. Challenge
+        yourself, think outside the box, and escape the mysteries that await!
+    </p>
+</div>
+
+<style>
+    /* Styles remain unchanged for consistency */
+    div {
+        margin: 0 3rem;
+        padding: 0 1rem;
+        text-align: justify;
+    }
+
+    img {
+        width: 100%;
+        margin: 0;
+        padding: 0;
+    }
+
+    p {
+        font-size: 1.1rem;
+    }
+
+    strong {
+        font-weight: bold;
+        color: #222;
+    }
+
+    em {
+        font-style: italic;
+        font-weight: bold;
+    }
+
+    ul {
+        list-style-type: disc;
+        margin-left: 2rem;
+    }
+
+    li {
+        margin-bottom: 0.5rem;
+    }
+
+    h2 {
+        font-size: 1.3rem;
+        font-weight: bold;
+        margin-top: 1.5rem;
+        margin-bottom: 1rem;
+    }
+
+    .contact-links {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-evenly;
+        margin-bottom: 2rem;
+    }
+
+    .contact-links a {
+        flex: 0 0 calc(25% - 1.5rem);
+        margin: 0 0.75rem;
+        margin-top: 2rem;
+    }
+
+    .contact-links img {
+        width: 100%;
+        height: auto;
+        border-radius: 50%;
+        box-shadow: 0.4rem 0.4rem 0.4rem rgba(0, 0, 0, 0.5);
+        transition: transform 0.3s ease-in-out, box-shadow 0.3s ease-in-out;
+    }
+
+    .contact-links img:hover {
+        transform: scale(1.2);
+        box-shadow: 0.8rem 0.8rem 0.8rem rgba(0, 0, 0, 0.5);
+    }
+
+    @media (min-width: 800px) {
+        /* Apply styles when the window width exceeds 800px */
+        .contact-links a {
+            max-width: 130px; /* Set the max-width as per your requirement */
+        }
+    }
+</style>
