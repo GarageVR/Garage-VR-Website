17c4ba84eddbf8ec4cf74e04426367e0e1a7b426
Fuat Can Akgün <fuatcanakgun@gmail.com>
2023-11-14 22:25:29 +0300

    Add beyond the abyss placeholder

--- src/routes/beyondTheAbyss/+page.svelte
+++ src/routes/beyondTheAbyss/+page.svelte
@@ -1,3 +1,28 @@
 <title>GarageVR: Beyond The Abyss</title>
 
-<h1>Beyond The Abyss</h1>
+<div>
+    <h1>Beyond The Abyss</h1>
+
+    <h2>This game is currently in development.</h2>
+    <h2>Please check back later for more...</h2>
+</div>
+
+<style>
+    div {
+        margin: 0 3rem;
+        padding: 0 1rem;
+    }
+
+    h1 {
+        font-size: 2.5rem;
+        font-weight: 700;
+        margin-bottom: 1rem;
+    }
+
+    h2 {
+        font-size: 1.5rem;
+        font-weight: 500;
+        margin-bottom: 1rem;
+        font-style: italic;
+    }
+</style>
