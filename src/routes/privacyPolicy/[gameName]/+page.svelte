ca3037383bb4f51ed0131c26793dedd4a532dbd0
Fuat Can Akgün <fuatcanakgun@gmail.com>
2023-12-28 23:10:21 +0300

    Update +page.svelte

--- src/routes/privacyPolicy/[gameName]/+page.svelte
+++ src/routes/privacyPolicy/[gameName]/+page.svelte
@@ -8,8 +8,8 @@
 <div>
     <h1>Privacy Policy</h1>
     <p>
-        <strong>{data.gameName}</strong> is a virtual reality (VR) game. This Privacy Policy describes
-        how we use, share, and protect information that we collect from players of <strong>{data.gameName}.</strong>
+        <strong>{data.gameName}</strong> is a virtual reality (VR) game available through Oculus. This Privacy Policy describes
+        how we, GarageVR, use, share, and protect information that we collect from players of <strong>{data.gameName}.</strong>
     </p>
 
     <p>
@@ -18,8 +18,8 @@
 
     <p>
         We may collect anonymized information from your play of <strong>{data.gameName}.</strong>
-        Please note, <strong>{data.gameName}</strong> does not collect the player’s name, physical address,
-        contact information, or any geolocation information.
+        While playing, <strong>{data.gameName}</strong> does not collect personal identifiers such as the player’s name, physical address,
+        contact information, or geolocation. However, we do collect information related to in-app purchases to ensure the stability of player transactions within the system. Additionally, game logs are collected for debugging purposes.
     </p>
 
     <p>
@@ -27,10 +27,59 @@
     </p>
 
     <p>
-        We use the information that we collect to analyze and improve our game and
-        support the internal operations of <strong>{data.gameName}.</strong> We aggregate all
-        information collected so that it cannot be used to identify any particular
-        player.
+        We utilize the information collected, including in-app purchase details and game logs, to analyze and enhance the gaming experience of <strong>{data.gameName}.</strong> All data is aggregated to prevent identification of individual players and is employed to improve the overall quality and internal operations of the game.
+    </p>
+
+    <p>
+        <strong>LEGAL BASIS AND PURPOSE OF PROCESSING</strong>
+    </p>
+
+    <p>
+        We process personal data based on the following legal bases and purposes:
+    </p>
+        <ul>
+            <li><strong>Performance of Agreements:</strong> Processing to fulfill obligations in agreements with players, such as in-app purchases.</li>
+            <li><strong>Legal Obligations:</strong> Processing to comply with legal obligations, e.g., preventing collection of personal data from minors.</li>
+            <li><strong>Consent:</strong> Processing with user consent, such as subscribing to newsletters.</li>
+            <li><strong>Legitimate Interest:</strong> Processing based on our legitimate interest, e.g., improving services, providing personalized offers, and organizing competitions.</li>
+        </ul>
+
+    <p>
+        <strong>DISCLOSURE OF YOUR PERSONAL INFORMATION</strong>
+    </p>
+
+    <p>
+        We do not sell personal information. However, we may disclose it to third-party recipients, including but not limited to:
+    </p>
+        <ul>
+            <li>Zendesk for support desk services.</li>
+            <li>DocuSign and Scrive for IT and communications support.</li>
+            <li>Google Cloud, Apple, Meta, Pico, Playfab, Unity for service, data, and cloud storage.</li>
+            <li>Mailchimp and Gleam for marketing services.</li>
+        </ul>
+
+    <p>
+        <strong>LOCATION OF PROCESSING</strong>
+    </p>
+
+    <p>
+        In some cases, personal data may be transferred to third parties located outside the EU/EEA. Any such transfer will adhere to the necessary safeguards, including EU Standard Contractual Clauses, to ensure data protection compliance.
+    </p>
+
+    <p>
+        <strong>RETENTION PERIOD OF PERSONAL DATA</strong>
+    </p>
+
+    <p>
+        We will not retain personal data longer than necessary for the purposes described unless required by Data Protection Legislation. Personal data may be retained for legal, compliance, or defense purposes.
+    </p>
+
+    <p>
+        <strong>DATA SUBJECT’S RIGHTS</strong>
+    </p>
+
+    <p>
+        Users have specific rights regarding their personal data, including the right to correction, objection, erasure, restriction, information, access, and data portability. Requests can be made by contacting us at privacy@garagevr.co.
     </p>
 
     <p>
@@ -38,13 +87,7 @@
     </p>
 
     <p>
-        We may revise this Privacy Policy, for example, to reflect changes in the
-        law, our practices, or the features of <strong>{data.gameName}.</strong> These changes will
-        be effective immediately upon posting. As a result, you should periodically
-        review the Privacy Policy so that you are up to date on our most current
-        policies and practices. By playing <strong>{data.gameName},</strong> you agree to the terms
-        and conditions of this Privacy Policy, and, by continuing to play <strong>{data.gameName}</strong>
-        after changes have been made, you agree to the changes.
+        We may revise this Privacy Policy to reflect changes in law, practices, or features of <strong>{data.gameName}.</strong> Changes will be effective immediately upon posting. Players are encouraged to review the Privacy Policy periodically. Continued play of <strong>{data.gameName}</strong> signifies agreement to the terms and any changes.
     </p>
 
     <p>
@@ -55,6 +98,7 @@
         If you have any questions or concerns regarding this policy, please contact
         us at: <a href="mailto:hey@garagevr.co">hey@garagevr.co</a>
     </p>
+
 </div>
 
 <style>
